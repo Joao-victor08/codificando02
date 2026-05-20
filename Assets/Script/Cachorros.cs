6cf3ad474cd7dd03749df871471b48c0dcba9828
Joao-victor08 <joao_caceres@estudante.sesisenai.org.br>
2026-05-19 21:31:56 -0300

    Atributo sono adiciona

--- Assets/Script/Cachorros.cs
+++ Assets/Script/Cachorros.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class Cachorros : MonoBehaviour
 {
     public string nome;
-
+    public bool sono;
  
 
     private void Start()
     {
            Cachorros pitbul = new Cachorros();
         pitbul.nome = "bob";
-
+        pitbul.sono = true;
     }
 }
